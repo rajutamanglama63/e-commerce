906af41d605115fb6fcfa3d8289abe90bdf3a8a8
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-08 15:34:49 +0545

    feat: utils folder and its utility files created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const app = require("./app"); // the actual Express application
+const config = require("./utils/config");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
---
+++ server/utils/config.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+module.exports = {
+  DATABASE_URL,
+  PORT,
+};
