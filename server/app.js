4ac89f2b7cb11bc35e0a5016402e8a1a0e7c6df1
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-13 17:17:30 +0545

    feat: img upload in cloudinary done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const path = require("path");
 
 const messagesRouter = require("./controllers/messagesRouter");
+const productsRouter = require("./controllers/productsRouter");
 const userRouter = require("./controllers/user");
 
 const middleware = require("./utils/middleware");
+
+dotenv.config();
 
 const app = express();
 
@@ -13,10 +18,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
 app.use(middleware.tokenExtractor);
 
 app.use("/api/messages", messagesRouter);
 app.use("/api/users", userRouter);
+app.use("/api/products", productsRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unKnownEndPoint);
---
+++ server/controllers/productsRouter.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const cloudinary = require("cloudinary");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+const Product = require("../models/product");
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+router.post("/upload", async (req, res, next) => {
+  try {
+    // const pic = req.files;
+    // console.log(pic);
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ msg: "No files were uploaded." });
+    }
+
+    const file = req.files.file;
+
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+      removeTem(file.tempFilePath);
+      return res.status(400).json({ msg: "Invalid file format." });
+    }
+
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      {
+        folder: "electronics-shop",
+      },
+      async (err, result) => {
+        if (err) throw err;
+
+        removeTem(file.tempFilePath);
+
+        res
+          .status(200)
+          .json({ publicId: result.public_id, url: result.secure_url });
+      }
+    );
+  } catch (error) {
+    next(error);
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+const removeTem = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
+
+router.post("/", async (req, res, next) => {
+  const {
+    price,
+    product_name,
+    specification,
+    quantity,
+    img_publicId,
+    img_url,
+  } = req.body;
+
+  if (
+    !price ||
+    !product_name ||
+    !specification ||
+    !quantity ||
+    !img_publicId ||
+    !img_url
+  ) {
+    return res.status(400).json({ msg: "All fields are require!" });
+  }
+
+  const newProduct = await Product.create({
+    price,
+    product_name,
+    specification,
+    quantity,
+    img_publicId,
+    img_url,
+  });
+
+  res.status(200).json(newProduct);
+});
+
+module.exports = router;
