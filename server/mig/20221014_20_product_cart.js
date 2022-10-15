0a3d6e71489cb9a90de46ade140e5115bbc69f7f
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-15 19:13:15 +0545

    feat: product & catagory table re-created

---
+++ server/mig/20221014_20_product_cart.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("carts", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable("products", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      product_name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      specification: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+    await queryInterface.addColumn("carts", "product_id", {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "products", key: "id" },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("carts");
+    await queryInterface.dropTable("products");
+  },
+};
---
+++ server/mig/20221014_21_user_cart.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("carts", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    });
+    await queryInterface.createTable("users", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    });
+    await queryInterface.addColumn("carts", "user_id", {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("carts");
+    await queryInterface.dropTable("users");
+  },
+};
