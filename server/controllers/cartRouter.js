712df501c5d8c05df6ae8cf2c55dc4353c1d864b
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-15 15:59:03 +0545

    feat: cartRouter controller re-coded

--- server/controllers/cartRouter.js
+++ server/controllers/cartRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 const Cart = require("../models/cart");
 const Product = require("../models/product");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -10,10 +13,22 @@ router.post("/", async (req, res, next) => {
 
     const product_item = await Product.findByPk(product_id);
 
+    const decodedUser = jwt.verify(req.user, config.SECRET);
+
+    if (!decodedUser.id) {
+      return res.status(401).json({ err: "token missing or invalid." });
+    }
+
+    const user = await User.findByPk(decodedUser.id);
+
+    if (!user) {
+      return res.status(401).json({ err: "token missing or invalid." });
+    }
+
     const newCartData = await Cart.create({
       quantity,
       product_id: product_item,
-      user_id: req.decodedToken.id,
+      user_id: user.id,
     });
 
     res.status(200).json({ newCartData });
