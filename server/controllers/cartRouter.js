f6b41e842c20a453f7f86561e3040192a8e989b6
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-17 21:17:26 +0545

    feat: individual cart router updated

--- server/controllers/cartRouter.js
+++ server/controllers/cartRouter.js
@@ -68,8 +68,10 @@ router.get("/:id", async (req, res, next) => {
     allCartItems.forEach((item) => {
       if (item.userId.toString() === req.params.id) {
         return res.status(200).json({ item });
-      } else {
-        return res.status(500).json({ msg: "You have no items in cart." });
+      } else if (res.headersSent !== true) {
+        return res
+          .status(500)
+          .json({ msg: "You've no item listed in your cart yet." });
       }
     });
   } catch (error) {
