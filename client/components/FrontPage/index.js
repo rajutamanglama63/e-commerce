1c3ea64b33ff9d2fe11d63b682d303e7bcdbd29f
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-18 21:20:30 +0545

    feat: styling card

--- client/components/FrontPage/index.js
+++ client/components/FrontPage/index.js
@@ -13,12 +13,32 @@ const FrontPage = () => (
   //   </div>
   // </div>
 
-  <div className="wrapper">
-    <div>cards</div>
-    <div>cards</div>
-    <div>cards</div>
-    <div>cards</div>
-    <div>cards</div>
+  <div className="wrapper region-md">
+    <div className="grid grid-4up">
+      <div className="card-border card-padding">
+        <img
+          src="https://res.cloudinary.com/dydwwtmnj/image/upload/v1665655496/electronics-shop/vfhxxn3awxfynjku3aby.jpg"
+          alt="watch"
+        />
+        <span className="name">Siko 5</span>
+        <div className="price">Rs 170</div>
+        <div>specification, feature, discription and it's uniqueness</div>
+        <Link to="/#">view</Link>
+      </div>
+      <div className="card-border card-padding">
+        <img
+          src="https://res.cloudinary.com/dydwwtmnj/image/upload/v1665655496/electronics-shop/vfhxxn3awxfynjku3aby.jpg"
+          alt="watch"
+        />
+        <span className="name">Rado</span>
+        <div className="price">Rs 200</div>
+        <div>specification, feature</div>
+        <Link to="/#">view</Link>
+      </div>
+      <div>cards</div>
+      <div>cards</div>
+      <div>cards</div>
+    </div>
   </div>
 );
 
