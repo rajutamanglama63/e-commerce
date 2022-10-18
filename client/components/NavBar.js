10ff7bb527206a3f6b580fbb25cb9732ffed12df
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-18 20:04:09 +0545

    feat: logo changed in navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const NavBar = () => (
   <div className="wrapper">
     <div className="flex split-pair align-center region-tn">
-      <img src={toskaLogo} alt="toska" className="logo" />
+      {/* <img src={toskaLogo} alt="toska" className="logo" /> */}
+      <h4 className="logo">eGadgets</h4>
       <div className="flex gap-2">
         <Link to="/" className="text-link">
           Home
