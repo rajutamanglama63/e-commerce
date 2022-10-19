5e5f5143e704e36da98c9944dd55c0e159daa6dc
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-19 15:15:06 +0545

    feat: cart icon added in navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,4 +1,5 @@
-import toskaLogo from "../assets/images/toskalogo_color.svg";
+// import toskaLogo from "../assets/images/toskalogo_color.svg";
+import cart from "../assets/images/shopping-cart.png";
 import { Link } from "react-router-dom";
 
 const NavBar = () => (
@@ -6,12 +7,18 @@ const NavBar = () => (
     <div className="flex split-pair align-center region-tn">
       {/* <img src={toskaLogo} alt="toska" className="logo" /> */}
       <h4 className="logo">eGadgets</h4>
-      <div className="flex gap-2">
+      <div className="flex gap-2 align-center">
+        <div className="cart-sec">
+          <Link to="/#" className="text-link ">
+            <img src={cart} alt="cart" className="cart" />
+          </Link>
+          <span className="item-counter">12</span>
+        </div>
         <Link to="/" className="text-link">
           Home
         </Link>
-        <Link to="/messages" className="text-link">
-          Messages
+        <Link to="/#" className="text-link">
+          Login
         </Link>
       </div>
     </div>
