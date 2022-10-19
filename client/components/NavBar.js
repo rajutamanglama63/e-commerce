a62cb0e5ee273b1c4cd4dbb17fee730a8ddbe07c
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-19 16:28:34 +0545

    feat: cart icon maintained properly

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,16 +1,18 @@
 // import toskaLogo from "../assets/images/toskalogo_color.svg";
 import cart from "../assets/images/shopping-cart.png";
 import { Link } from "react-router-dom";
+import { GrCart } from "react-icons/gr";
 
 const NavBar = () => (
   <div className="wrapper">
     <div className="flex split-pair align-center region-tn">
       {/* <img src={toskaLogo} alt="toska" className="logo" /> */}
-      <h4 className="logo">eGadgets</h4>
+      <div className="logo">eGadgets</div>
       <div className="flex gap-2 align-center">
         <div className="cart-sec">
-          <Link to="/#" className="text-link ">
-            <img src={cart} alt="cart" className="cart" />
+          <Link to="/#" className="text-link align-center">
+            {/* <img src={cart} alt="cart" className="cart" /> */}
+            {<GrCart className="cart" />}
           </Link>
           <span className="item-counter">12</span>
         </div>
