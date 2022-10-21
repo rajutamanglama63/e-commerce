bc85ea1dd6cca78fcce489c3f42168682f267db0
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-21 14:57:09 +0545

    feat: style added to shipping form

--- client/components/Router.js
+++ client/components/Router.js
@@ -4,12 +4,14 @@ import Cart from "./Cart";
 import FrontPage from "./FrontPage";
 import MessageView from "./MessageView";
 import ProductDetail from "./ProductDetail";
+import ShippingAddress from "./ShippingAddress.js";
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<FrontPage />} />
     <Route path="/detail" element={<ProductDetail />} />
     <Route path="/cart" element={<Cart />} />
+    <Route path="/shipping" element={<ShippingAddress />} />
     <Route path="/messages" element={<MessageView />} />
   </Routes>
 );
--- client/components/ShippingAddress.js/index.js
+++ client/components/ShippingAddress.js/index.js
@@ -6,9 +6,21 @@ const ShippingAddress = () => {
       <div className="container container">
         <h4 className="h6 region-tn">Shipping address</h4>
         <form>
-          <input className="input-field" type="text" placeholder="Country" />
-          <input className="input-field" type="text" placeholder="City" />
-          <input className="input-field" type="text" placeholder="Place" />
+          <input
+            className="input-field border-line region-margin-tn paragraph"
+            type="text"
+            placeholder="Country"
+          />
+          <input
+            className="input-field border-line region-margin-tn paragraph"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="input-field border-line region-margin-tn paragraph"
+            type="text"
+            placeholder="Place"
+          />
         </form>
         <button className="btn">Continue</button>
       </div>
