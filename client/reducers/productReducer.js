f6d258036a4895e9af6983eb685cebe09041ef77
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-10-27 19:00:07 +0545

    feat: reducer created to get all products

---
+++ client/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { productServices } from "../services/products";
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: [],
+  reducers: {
+    setProducts(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setProducts } = productSlice.actions;
+
+export const initializeProducts = () => {
+  return async (dispatch) => {
+    const allProductItem = await productServices.getProducts();
+
+    dispatch(setProducts(allProductItem.data));
+  };
+};
